78496e15e809a0269bdfdb6b937a2b8bcad0ef59
Diyanti <diyantiyan51@gmail.com>
2018-07-16 17:34:10 +0700

    hapus data

--- perpus/download.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$file_url = $_GET['id'];
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-readfile($file_url);
-?>
